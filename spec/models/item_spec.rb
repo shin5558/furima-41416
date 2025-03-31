5c6095b7e5ffcb1460bf8f2316e572ca79a460f6
shin5558 <shin.5558@gmail.com>
2025-03-31 10:01:16 +0900

    修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -12,6 +12,12 @@ RSpec.describe Item, type: :model do
   end
 
   context '出品できないとき' do
+    it 'userが紐付いていなければ保存できない' do
+      @item.user = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include('User must exist')
+    end
+
     it '画像が空では保存できない' do
       @item.image = nil
       @item.valid?
