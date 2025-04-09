e35fc1cfde67ce333307fcf3d10fa9ad02ac7c2d
shin5558 <shin.5558@gmail.com>
2025-04-09 10:19:34 +0900

    商品情報編集機能提出前

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,15 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_item, only: [:edit, :update, :show]
+  before_action :move_to_root, only: [:edit, :update]
+
+  def move_to_root
+    redirect_to root_path if current_user != @item.user
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end  
 
   def index
     @items = Item.order(created_at: :desc)
@@ -21,6 +31,25 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
+  end
+
+  # 編集画面表示
+  def edit
+
+  end
+
+  # 更新処理
+  def update
+    @item = Item.find(params[:id])
+    if current_user == @item.user
+      if @item.update(item_params)
+        redirect_to item_path(@item), notice: '更新が完了しました'
+      else
+        render :edit
+      end
+    else
+      redirect_to root_path
+    end
   end
 
   private
